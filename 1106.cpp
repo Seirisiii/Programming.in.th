eadaaae2b83b646ba0560b8d22971aeb69800923
Thitikorn Kham-ai <thitikorn.tk2550@gmail.com>
2024-04-14 21:11:09 +0700

    all commit

--- 1106.cpp
+++ 1106.cpp
@@ -1,29 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 int n,k;
-queue<int> num;
+vector<int> v;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cin>>n>>k;
-    for(int i=k+1;i<=n;++i){
-        num.push(i);
+    for(int i=1;i<=n;++i){
+        v.push_back(i);
     }
-    for(int i=1;i<k;++i){
-        num.push(i);
+    int idx=0;
+    while(v.size()>1){
+        int tmp=k;
+        k=v[(idx+k-1)%v.size()];
+        idx=(idx+tmp-1)%(v.size());
+        v.erase(v.begin()+idx);
     }
-    while(num.size()>1){
-        for(int j=1;j<=k;++j){
-            if(j!=k)num.push(num.front());
-            else{
-                k=num.front();
-                num.pop();
-                break;
-            }
-			num.pop();
-		}
-    }
-    cout<<num.front();
+    cout<<v[0];
     return 0;
 }
 
