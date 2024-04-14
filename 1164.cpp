c7a9aec7e67b7321ae3a5a438e2b3607c4fa14cf
Thitikorn Kham-ai <thitikorn.tk2550@gmail.com>
2024-04-14 15:16:03 +0700

    all commit

---
+++ 1164.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+pair<int,int> p[1000000];
+vector<pair<int,int>> v;
+bool compare(pair<int,int> a,pair<int,int> b){
+    if(a.first!=b.first)return a.first>b.first;
+    return a.second<b.second;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>n;
+    for(int i=0;i<n;++i){
+        cin>>p[i].first>>p[i].second;
+    }
+    sort(p,p+n,compare);
+    int mx=p[0].second;
+    for(int i=0;i<n;++i){
+        if(mx<=p[i].second){
+            mx=p[i].second;
+            v.push_back(p[i]);
+        }
+    }
+    sort(v.begin(),v.end(),compare);
+    reverse(v.begin(),v.end());
+    for(auto i:v){
+        cout<<i.first<<" "<<i.second<<"\n";
+    }
+    return 0;
+}
