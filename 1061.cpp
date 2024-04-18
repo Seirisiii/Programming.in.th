260a1c5190ee2a5513b9757306b3ce71e3035585
Thitikorn Kham-ai <thitikorn.tk2550@gmail.com>
2024-04-18 23:06:32 +0700

    Memory limit

--- 1061.cpp
+++ 1061.cpp
@@ -3,14 +3,8 @@ using namespace std;
 int m;
 int ii,jj;
 int arr[20][20];
-int dfs(int i,int j){
-    int res=arr[i][j];
-    if(i-1>=0)if(arr[i-1][j]>arr[i][j] and arr[i-1][j]!=100)res=max(dfs(i-1,j),res);
-    if(i+1<m)if(arr[i+1][j]>arr[i][j] and arr[i+1][j]!=100)res=max(dfs(i+1,j),res);
-    if(j-1>=0)if(arr[i][j-1]>arr[i][j] and arr[i][j-1]!=100)res=max(dfs(i,j-1),res);
-    if(j+1<m)if(arr[i][j+1]>arr[i][j] and arr[i][j+1]!=100)res=max(dfs(i,j+1),res);
-    return res;
-}
+queue<pair<int,int>> q;
+bool visited[20][20];
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -20,7 +14,21 @@ int main(){
         for(int j=0;j<m;++j){
             cin>>arr[i][j];
         }
+    }    
+    int res=arr[ii][jj];
+    q.push({ii,jj});
+    while(!q.empty()){
+        int i=q.front().first;
+        int j=q.front().second;
+        q.pop();
+        if(visited[i][j])continue;
+        visited[i][j]=true;
+        res=max(arr[i][j],res);
+        if(i-1>=0)if(arr[i-1][j]>arr[i][j] and arr[i-1][j]!=100)q.push({i-1,j});
+        if(i+1<m)if(arr[i+1][j]>arr[i][j] and arr[i+1][j]!=100)q.push({i+1,j});
+        if(j-1>=0)if(arr[i][j-1]>arr[i][j] and arr[i][j-1]!=100)q.push({i,j-1});
+        if(j+1<m)if(arr[i][j+1]>arr[i][j] and arr[i][j+1]!=100)q.push({i,j+1});
     }
-    cout<<dfs(ii,jj);
+    cout<<res;
     return 0;
 }
