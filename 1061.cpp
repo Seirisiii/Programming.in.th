657305117b5c81c6f6cb2c4b82f2f70aad0ae659
Thitikorn Kham-ai <thitikorn.tk2550@gmail.com>
2024-04-18 20:54:34 +0700

    1061 commit

--- 1061.cpp
+++ 1061.cpp
@@ -1,30 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 int m;
-int x,y;
-int table[21][21];
-int mx;
-void solve(int i,int j,int x){
-    if(i<1 or i>m or j<1 or j>m or table[i][j]==100 or table[i][j]<=x)return ;
-    mx=max(mx,table[i][j]);
-    x=table[i][j];
-    solve(i-1,j,x);
-    solve(i,j-1,x);
-    solve(i+1,j,x);
-    solve(i,j+1,x);
-    return ;
+int ii,jj;
+int arr[20][20];
+int dfs(int i,int j){
+    int res=arr[i][j];
+    if(i-1>=0)if(arr[i-1][j]>arr[i][j] and arr[i-1][j]!=100)res=max(dfs(i-1,j),res);
+    if(i+1<m)if(arr[i+1][j]>arr[i][j] and arr[i+1][j]!=100)res=max(dfs(i+1,j),res);
+    if(j-1>=0)if(arr[i][j-1]>arr[i][j] and arr[i][j-1]!=100)res=max(dfs(i,j-1),res);
+    if(j+1<m)if(arr[i][j+1]>arr[i][j] and arr[i][j+1]!=100)res=max(dfs(i,j+1),res);
+    return res;
 }
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    cin>>m;
-    cin>>x>>y;
-    for(int i=1;i<=m;++i){
-        for(int j=1;j<=m;++j){
-            cin>>table[i][j];
+    cin>>m>>jj>>ii;
+    --ii,--jj;
+    for(int i=0;i<m;++i){
+        for(int j=0;j<m;++j){
+            cin>>arr[i][j];
         }
     }
-    solve(y,x,-6);
-    cout<<mx;
+    cout<<dfs(ii,jj);
     return 0;
 }
