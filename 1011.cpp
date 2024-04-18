06e10cedf48cbdf0671aa377da9eb287ad138f75
Thitikorn Kham-ai <thitikorn.tk2550@gmail.com>
2024-04-18 22:24:16 +0700

    bitch commit

--- 1011.cpp
+++ 1011.cpp
@@ -3,102 +3,75 @@ using namespace std;
 int m,n;
 int k;
 int score;
-char block[100][100];
+char arr[100][100];
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cin>>m>>n;
     for(int i=0;i<m;++i){
         for(int j=0;j<n;++j){
-            cin>>block[i][j];
+            cin>>arr[i][j];
         }
     }
     cin>>k;
     while(k--){
-        int row,col;
+        int r,c;
 		char direct;
-		cin>>row>>col>>direct;
-		if(block[row][col]=='-' or block[row][col]=='#'){
-			score-=5;
-			continue;
-		}
-		int r=row,c=col;
-		if(direct=='L')--col;
-		else if(direct=='R')++col;
-		if(block[row][col]=='#' or block[row][col]!='-'){
-			score-=5;
-			continue;
-		}
-		while(block[row+1][col]=='-'){
-            ++row;
-		}
-		if(block[row][col]!='#')swap(block[r][c],block[row][col]);
-		bool check=false;
-		if(block[row-1][col]==block[row][col]){
-            score+=5;
-			block[row-1][col]='-';
-			check=true;
-		}
-		if(block[row+1][col]==block[row][col]){
-            score+=5;
-			block[row+1][col]='-';
-			check=true;
-		}
-		if(block[row][col-1]==block[row][col]){
-            score+=5;
-			block[row][col+1]='-';
-			check=true;
-		}
-		if(block[row][col+1]==block[row][col]){
-            score+=5;
-			block[row][col+1]='-';
-			check=true;
-		}
-		if(check){
-            score+=5;
-			block[row][col]='-';
-		}
-		for(int i=0;i<m;++i){
-			for(int j=0;j<n;++j){
-				int rw=i;
-				if(block[i][j]!='#' and block[i][j]!='-'){
-					while(block[rw+1][j]=='-'){
-                        ++rw;
-					}
-					swap(block[rw][j],block[i][j]);
-					if(block[rw-1][j]==block[rw][j]){
-                        score+=5;
-                        block[rw-1][j]='-';
-                        check=true;
+		cin>>r>>c>>direct;
+        if(arr[r][c]>='A' and arr[r][c]<='Z'){
+            if(direct=='L'){
+                if(arr[r][c-1]=='-')swap(arr[r][c-1],arr[r][c]);
+                else score-=5;
+            }
+            else{
+                if(arr[r][c+1]=='-')swap(arr[r][c+1],arr[r][c]);
+                else score-=5;
+            }
+        }
+        else score-=5;
+        for(int i=m-1;i>=0;--i){
+            for(int j=n-1;j>=0;--j){
+                if(arr[i][j]>='A' and arr[i][j]<='Z'){
+                    while(arr[i+1][j]=='-'){
+                        arr[i+1][j]=arr[i][j];
+                        arr[i][j]='-';
+                        ++i;
                     }
-                    if(block[rw+1][j]==block[rw][j]){
-                        score+=5;
-                        block[rw+1][j]='-';
-                        check=true;
+                    if(arr[i][j]>='A' and arr[i][j]<='Z'){
+                        bool check=false;
+                        if(arr[i][j]==arr[i-1][j]){
+                            arr[i-1][j]='-';
+                            score+=5;
+                            check=true;
+                        }
+                        if(arr[i][j]==arr[i][j+1]){
+                            arr[i][j+1]='-';
+                            score+=5;
+                            check=true;
+                        }
+                        if(arr[i][j]==arr[i+1][j]){
+                            arr[i+1][j]='-';
+                            score+=5;
+                            check=true;
+                        }
+                        if(arr[i][j]==arr[i][j-1]){
+                            arr[i][j-1]='-';
+                            score+=5;
+                            check=true;
+                        }
+                        if(check){
+                            arr[i][j]='-';
+                            score+=5;
+                        }
                     }
-                    if(block[rw][j-1]==block[rw][j]){
-                        score+=5;
-                        block[rw][j+1]='-';
-                        check=true;
-                    }
-                    if(block[rw][j+1]==block[rw][j]){
-                        score+=5;
-                        block[rw][j+1]='-';
-                        check=true;
-                    }
-                    if(check){
-                        score+=5;
-                        block[rw][j]='-';
-                    }
-				}
-				check=false;
-			}
-		}
+                }
+            }
+        }
     }
     cout<<score<<"\n";
     for(int i=0;i<m;++i){
         for(int j=0;j<n;++j){
-            cout<<block[i][j]<<" ";
+            cout<<arr[i][j]<<" ";
         }
         cout<<"\n";
     }
