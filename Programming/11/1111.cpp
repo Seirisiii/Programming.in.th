fcf061750d112180c917f7e21ec06c8a47677519
Thitikorn Kham-ai <thitikorn.tk2550@gmail.com>
2024-04-19 19:17:26 +0700

    1111 commit

--- Programming/11/1111.cpp
+++ Programming/11/1111.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int n;
 vector<pair<int,int>> adj[100001];
-int dp[100001];
+int dist[100001];
 int mx;
 int main(){
     ios_base::sync_with_stdio(false);
@@ -11,12 +11,12 @@ int main(){
     for(int i=0;i<n-1;++i){
         int x,y,z;
         cin>>x>>y>>z;
-        adj[x-1].push_back({y-1,z});
+        adj[x-1].push_back(y-1,z);
     }
     for(int i=0;i<n;++i){
         for(auto j:adj[i]){
-            dp[j.first]=max(dp[j.first],dp[i]+j.second);
-            mx=max(dp[j.first],mx);
+            dist[j.first]=max(dist[j.first],dist[i]+j.second);
+            mx=max(dist[j.first],mx);
         }
     }
     cout<<mx;
