c5c3257202fdbdc6b82e27a5dd996a988a221018
Thitikorn Kham-ai <thitikorn.tk2550@gmail.com>
2024-04-21 23:18:56 +0700

    1090 commit

--- Programming/10/1090.cpp
+++ Programming/10/1090.cpp
@@ -1,20 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n;
-long long tail[1000000];
-long long a,ans;
-unordered_map<long long,long long> tails;
+long long n,a;
+long long arr[1000005];
+long long ans;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cin>>n;
     for(int i=0;i<n;++i){
-        cin>>tail[i];
+        int x;
+        cin>>x;
+        ++arr[x];
     }
     cin>>a;
-    for(int i=0;i<n;++i){
-        ans+=tails[a-tail[i]];
-        ++tails[tail[i]];
+    for(int i=0;i<=a/2 and i<=100000;++i){
+        if(i==a-i)ans+=(arr[i]*(arr[i]-1)/2);
+        else ans+=arr[i]*arr[a-i];
     }
     cout<<ans;
     return 0;
