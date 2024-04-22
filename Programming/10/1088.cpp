9ef06e11409dd45a06813550b8ae64580911e0c7
Thitikorn Kham-ai <thitikorn.tk2550@gmail.com>
2024-04-22 23:02:58 +0700

    1088 commit

---
+++ Programming/10/1088.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,m;
+char c;
+map<int,int> mp;
+int res;
+int sum;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>n>>m;
+    mp[n+1]=0;
+    while(m--){
+        int a,b;
+        cin>>a>>b;
+        ++mp[a];
+        --mp[b+1];
+    }
+    cin>>c;
+    if(c=='R')res=3;
+    else if(c=='G')res=4;
+    else if(c=='B')res=5;
+    int previdx=1;
+    int curr=0;
+    for(auto i:mp){
+        i.second%=3;
+        if(i.second<0)i.second+=3;
+        sum+=((res-curr)%3)*(i.first-previdx);
+        previdx=i.first;
+        curr=(curr+i.second)%3;
+    }
+    cout<<sum;
+    return 0;
+}
