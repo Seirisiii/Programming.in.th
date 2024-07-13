2215efe2f25c57d5404d4c46ede6647b1a1b0f7a
Thitikorn Kham-ai <thitikorn.tk2550@gmail.com>
2024-07-13 22:06:03 +0700

    TOI10 GOSCHOOL

--- TOI/10/toi10_goschool.cpp
+++ TOI/10/toi10_goschool.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int m,n;
 int o;
 bool check[51][51];
-int dp[51][51];
+long long dp[51][51];
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -12,21 +12,15 @@ int main() {
     while(o--){
         int a,b;
         cin>>a>>b;
-        check[a][b]=true;
+        check[b][a]=true;
     }
-    dp[1][1]=!check[1][1]?1:0;
-    for(int i=1;i<=m;++i){
-        for(int j=1;j<=n;++j){
+    dp[1][0]=1;
+    for(int i=1;i<=n;++i){
+        for(int j=1;j<=m;++j){
             if(check[i][j])continue;
-            dp[i][j]+=dp[i-1][j]+dp[i][j-1];
+            dp[i][j]=dp[i-1][j]+dp[i][j-1];
         }
     }
-    // for(int i=1;i<=m;++i){
-    //     for(int j=1;j<=n;++j){
-    //         cout<<dp[i][j]<<" ";
-    //     }
-    //     cout<<"\n";
-    // }
-    cout<<dp[m][n];
+    cout<<dp[n][m];
     return 0;
 }
