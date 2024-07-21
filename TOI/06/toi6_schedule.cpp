16f87c936dd6f7f17c34f94a92382bb9b4feaf16
Thitikorn Kham-ai <thitikorn.tk2550@gmail.com>
2024-07-21 18:47:01 +0700

    TOI6 Schedule

---
+++ TOI/06/toi6_schedule.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,k,m;
+priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>> pq;
+priority_queue<int,vector<int>,greater<int>> pq2;
+bool res[500000];
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>n>>k>>m;
+    for(int i=0;i<n;++i){
+        int s,f;
+        cin>>s>>f;
+        pq.emplace(s,f,i);
+    }
+    while(!pq.empty()){
+        auto [s,f,i]=pq.top();
+        pq.pop();
+        while(!pq2.empty()){
+            if(s>pq2.top())pq2.pop();
+            else break;
+        }
+        int sz=pq2.size();
+        if(sz<k){
+            res[i]=true;
+            pq2.emplace(f);
+        }
+    }
+    while(m--){
+        int x;
+        cin>>x;
+        --x;
+        if(res[x])cout<<"Y ";
+        else cout<<"N ";
+    }
+    return 0;
+}
